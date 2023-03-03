6f3b7b2c44ea8a16716c7e5653308d4cc362748f
CrezyDeveloper <122361875+CrezyDeveloper@users.noreply.github.com>
2023-03-03 16:01:12 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/crezydeveloperr/MDISK-SEARCH-BOT-2.git /MDISK-SEARCH-BOT-2
-cd /MDISK-SEARCH-BOT-2
+git clone https://github.com/CrezyDeveloper/Mdisk-search-botz.git /Mdisk-search-botz
+cd /Mdisk-search-botz
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
