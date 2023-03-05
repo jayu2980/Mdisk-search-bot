2724886adc27649cc371bd81e29acc0eceb54ba2
jayu2980 <124619960+jayu2980@users.noreply.github.com>
2023-03-05 07:31:49 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/CrezyDeveloper/Mdisk-search-botz.git /Mdisk-search-botz
-cd /Mdisk-search-botz
+git clone https://github.com/jayu2980/Mdisk-search-bot.git /Mdisk-search-bot
+cd /Mdisk-search-bot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
